48f9cc2092760ed56c411d82c46bea9e32d8e768
Olanrewaju Williams <owolabilanre2006@gmail.com>
2025-11-26 21:08:14 +0100

    Lanre sent a commit message -recieved

--- backend/src/db.js
+++ backend/src/db.js
@@ -2,12 +2,13 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   try {
-    await mongoose.connect(
-      "mongodb+srv://owolabilanre2006_db_user:<db_password>@cluster0.chiovt4.mongodb.net/?appName=Cluster0", 
-      { dbName: "small_business_platform" }
-    );
+    await mongoose.connect(process.env.MONGO_URI, {
+      dbName: "oja247",
+    });
+
     console.log("MongoDB connected successfully");
   } catch (error) {
     console.error("MongoDB connection failed:", error);
+    process.exit(1); // Optional: exit on failure
   }
 };
