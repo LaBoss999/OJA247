62050bc028ccdd4c7b0f682a74084377922b20f7
Olanrewaju Williams <owolabilanre2006@gmail.com>
2025-11-30 01:38:05 +0100

    Refactor: Comment out database connection and business data in clearBusinesses and seedBusinesses scripts; update README for project clarity

--- backend/clearBusinesses.js
+++ backend/clearBusinesses.js
@@ -1,23 +1,23 @@
-clearBusinesses.js
-import mongoose from "mongoose";
-import dotenv from "dotenv";
-import Business from "./src/models/Business.js"; // make sure this is ES Module (export default)
+// clearBusinesses.js
+// import mongoose from "mongoose";
+// import dotenv from "dotenv";
+// import Business from "./src/models/Business.js"; // make sure this is ES Module (export default)
 
-dotenv.config();
+// dotenv.config();
 
-const clearBusinesses = async () => {
-  try {
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log("MongoDB connected");
+// const clearBusinesses = async () => {
+//   try {
+//     await mongoose.connect(process.env.MONGO_URI);
+//     console.log("MongoDB connected");
 
-    const result = await Business.deleteMany({});
-    console.log(`Deleted ${result.deletedCount} businesses`);
+//     const result = await Business.deleteMany({});
+//     console.log(`Deleted ${result.deletedCount} businesses`);
 
-    await mongoose.disconnect();
-    console.log("MongoDB disconnected");
-  } catch (err) {
-    console.error("Error clearing businesses:", err);
-  }
-};
+//     await mongoose.disconnect();
+//     console.log("MongoDB disconnected");
+//   } catch (err) {
+//     console.error("Error clearing businesses:", err);
+//   }
+// };
 
-clearBusinesses();
+// clearBusinesses();
